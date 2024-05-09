44163a68f2c081b8fc13d05f71fb16a97635aa29
重永隆良 <kd1253598@st.kobedenshi.ac.jp>
2024-05-09 13:55:06 +0900

    データ送信

---
+++ public/get.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GETメソッドのデータ</title>
+</head>
+<body>
+<h4>0J01020 重永隆良</h4>
+    <p>GETメソッドで送られてきたパラメータ名dataの値は、
+        <?php
+        echo $_GET['data'] . " です。</p>";
+        ?>
+</body>
+</html>
